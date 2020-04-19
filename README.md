a33b2bf34fdefae10cdde286ba040e5b9bffe85b
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-19 15:05:04 -0600

    added outline to README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# employee-information-generator
+# Employee Information Generator 
+
+![Badge](https://img.shields.io/static/v1?label=License&message=MIT&color=9cf)
+
+### Table of Contents:
+
+## Description 
+
+## Installation
+
+## Usage
+
+## Credits
+
+## License 
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve("./output", "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -14,7 +14,7 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-//TODO call on the const to ask questions for each person! They all are different 
+
 
 
 // After the user has input all employees desired, call the `render` function (required
