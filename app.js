1e2bd6a400aa501be63cc2aca5dfd5866fad18ae
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-20 16:54:04 -0600

    updated apps for manager function in app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve("./output", "output");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -14,8 +15,69 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function basicEmployee() {
+
+    await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "What is your ID number?"
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email?"
+
+        },
 
 
+    ]).then(({ name, id, email }) => {
+        const newEmployee = new Employee(name, id, email)
+
+        return newEmployee;
+    }
+    )
+}
+
+basicEmployee.getName()
+basicEmployee.getEmail()
+basicEmployee.getID();
+
+function managerRender() {
+    await inquirer.prompt([
+
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "What is your office number?",
+            value: 0
+        },
+    ]).then((officeNumber) => {
+        const newManager = new Manager(officeNumber)
+        return newManager;
+    })
+
+}
+managerRender.getName();
+managerRender.getEmail();
+managerRender.getID();
+managerRender.getOffice();
+
+
+{
+    type: "checkbox",
+        message: "What is your role?",
+            name: "role",
+                choices: ["Employee", "Manager", "Engineer", "Intern"]
+
+},
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
